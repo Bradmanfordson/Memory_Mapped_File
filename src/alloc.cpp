a1af3643f034503aa6f9092442539ce5e9fe783f
ABradRoberts <bradr555@gmail.com>
2019-04-19 15:22:05 -0500

    Update Makefile and have alloc.cpp almost complete.

--- src/alloc.cpp
+++ src/alloc.cpp
@@ -1,60 +1,86 @@
-// TODO: Do i use a vector of structs OR do i need to something else with mmap?
-
-#include <sys/mman.h>  // mmap(), msync()
+#include <fcntl.h>
 #include <fstream>
 #include <iostream>
+#include <sys/stat.h>  // fstat
+#include <sys/types.h> // fstat
+#include <unistd.h>    // fstat
+#include <iostream>
 #include <vector>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <errno.h>
 
-using namespace std;
+void get_resources();
+void update_memory();
 
-struct mem_resource {
-    int type;
-    int num;
-};
-
-void read_file(vector<mem_resource> test);
-int num_lines();
-void print_mem(vector<mem_resource> test);
+char *mem_map;
+sem_t* semaphore;
 
 int main() {
-    // TODO...
-    cout << "alloc" << endl;
-    // vector<mem_resource> test;
-    // read_file(test);
-    // print_mem(test);
+    // Create mapped memory region
+    
+    bool sem_create = true;
 
+    // std::ifstream file("res.txt");
+    struct stat filestat;
+    int file = open("res.txt", O_RDWR | O_SYNC,S_IWRITE | S_IREAD); // Open file with int for fstat()
+
+    fstat(file, &filestat); // give the filestat struct stats about the file such as file size.
+    int size = filestat.st_size;
+
+	mem_map = (char*)mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, file, 0);
+
+    std::cout << size<< std::endl;
+    std::cout << mem_map << std::endl; // print everything in memory
+    
+    while(true){
+       get_resources();
+    }
+
+
+    close(file);
     return 0;
 }
 
-void read_file(vector<mem_resource> test) {
-    ifstream file("res.txt");
-    string line;
-    string tmp = "";
-    int i = 0;
+void get_resources(){
+    std::string answer;
+    std::cout << "Do you need to allocate resources? (y/n): ";
+    std::cin >> answer;
 
-    while (getline(file, line)) {  // keep reading until no more lines
-        // idk
+    if(answer.compare("y") == 0){
+        int type, num_units;
+
+        std::cout << "Type (integer): ";
+        std::cin >> type;
+        while(std::cin.fail()){
+            std::cout << "Please, enter an integer value for Type." << std::endl;
+            std::cin.clear();
+            std::cin.ignore(256, '\n');
+            std::cout << "Type (integer): ";
+            std::cin>>type;
+        }
+
+        std::cout << "Number of Units (integer): ";
+        std::cin >> num_units;
+        while(std::cin.fail()){
+            std::cout << "Please, enter an integer value for Number of Units." << std::endl;
+            std::cin.clear();
+            std::cin.ignore(256, '\n');
+            std::cout << "Number of Units (integer): ";
+            std::cin>>num_units;
+        }
+            std::cout <<"Type: " << type <<std::endl<< "Units: " << num_units << std::endl;
+        
+        update_memory();  
     }
-    file.close();
 }
 
-void print_mem(vector<mem_resource> test) {
-    vector<mem_resource>::iterator it;
-    for (it = test.begin(); it != test.end(); it++) {
-        cout << "test 1" << it->type << endl;
-        cout << "num" << it->num << endl;
-    }
-}
+void update_memory(){
+    sem_wait(semaphore);
 
-int num_lines() {
-    int lines = 0;
-    ifstream file("res.txt");
-    string line;
-
-    while (getline(file, line)) {
-        lines++;
-    }
-    file.close();
-
-    return lines;
+    
 }
